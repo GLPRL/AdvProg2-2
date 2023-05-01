8f8a6aeb031ef4d32f02d13a65358e4b55b70078
Gal Pearl <galpearl24@gmail.com>
2023-05-01 23:15:57 +0300

    Corrected grayed-out modal, small fix.

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -134,23 +134,23 @@ function ChatWindow() {
                         <button className="btn btn-success outbtn">Send</button>
                     </div>
                 </div>
-                <div className="modal fade text" id="exampleModal" tabIndex="-1" role="dialog"
-                     aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">Add new contact</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <input id="modalInput" placeholder="Contact's identifier"></input>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Add</button>
-                            </div>
+            </div>
+            <div className="modal fade text" id="exampleModal" tabIndex="-1" role="dialog"
+                 aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Add new contact</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <input id="modalInput" placeholder="Contact's identifier"></input>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Add</button>
                         </div>
                     </div>
                 </div>
