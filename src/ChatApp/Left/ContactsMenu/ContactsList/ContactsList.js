719afee78c9b5ec93b4ed7394eb3a8850cb25952
NaorGavriel <117026659+NaorGavriel@users.noreply.github.com>
2023-05-03 09:51:46 +0300

    trying to get contact data from js file

--- src/ChatApp/Left/ContactsMenu/ContactsList/ContactsList.js
+++ src/ChatApp/Left/ContactsMenu/ContactsList/ContactsList.js
@@ -1,17 +1,28 @@
 import profilePic3 from '../../../../profilePic3.jpg'
 import ContactItem from '../ContactItem/ContactItem';
 import monkeyPic from '../../../../monkeyprofilepic.jpg'
+import contacts from '../../../../contactList';
+import '../../../../chatWindow.css'
+
+
+
+
+
 /* Fetch usernames pics and dates */
 function ContactsList() {
-    return(
-        <>
-            <ContactItem userName={"user1"} profilePic={profilePic3} lastMsgDate={"10/10/1999 11:55 PM"}/>
-            <ContactItem userName={"user1"} profilePic={monkeyPic} lastMsgDate={"10/10/1999 11:55 PM"}/>
-            <ContactItem userName={"user1"} profilePic={monkeyPic} lastMsgDate={"10/10/1999 11:55 PM"}/>
-            <ContactItem userName={"user1"} profilePic={monkeyPic} lastMsgDate={"10/10/1999 11:55 PM"}/>
-            <ContactItem userName={"user1"} profilePic={monkeyPic} lastMsgDate={"10/10/1999 11:55 PM"}/>
-            <ContactItem userName={"user1"} profilePic={monkeyPic} lastMsgDate={"10/10/1999 11:55 PM"}/>
-        </>
-    )
+    
+        const allContacts = contacts.map(contact => 
+            <li key={contact.id} className='contacts'>
+            <tr className="table-info">
+                <td><img src={ profilePic3 } alt="" className="chat-profile-image rounded-circle"></img></td>
+                <td>{contact.name}</td>
+                <td><small>some date</small></td>
+            </tr>
+            </li>
+            );
+
+        return(
+        <ul className='contactUl'>{allContacts}</ul>
+        )
 }
 export default ContactsList;
---
+++ src/contactList.js
@@ -0,0 +1,16 @@
+
+
+const contacts = [
+    {id:1, name: 'Contact One'},
+    {id:2, name: 'Contact Two'},
+    {id:3, name: 'Contact Three'},
+    {id:4, name: 'Contact Four'},
+    {id:5, name: 'Contact Five'},
+    {id:1, name: 'Contact One'},
+    {id:2, name: 'Contact Two'},
+    {id:3, name: 'Contact Three'},
+    {id:4, name: 'Contact Four'},
+    {id:5, name: 'Contact Five'}
+]
+
+export default contacts
