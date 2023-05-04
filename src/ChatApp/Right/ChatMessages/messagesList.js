7a6d056ffdd7ce31c0f781fa4d877a92658b121e
NaorGavriel <117026659+NaorGavriel@users.noreply.github.com>
2023-05-04 14:53:18 +0300

    removed id in messages

--- src/ChatApp/Right/ChatMessages/messagesList.js
+++ src/ChatApp/Right/ChatMessages/messagesList.js
@@ -1,15 +1,15 @@
 const messages = [
-    {id: 1, text:"a message one" ,floatValue:"float-right"},
-    {id: 2, text:"a message one" ,floatValue:"float-right"},
-    {id: 3, text:"a kewer one" ,floatValue:"float-right"},
-    {id: 4, text:"a message one" ,floatValue:"float-right"},
-    {id: 5, text:"a message one" ,floatValue:"float-left"},
-    {id: 6, text:"a message rewrwe" ,floatValue:"float-right"},
-    {id: 7, text:"a message one" ,floatValue:"float-right"},
-    {id: 8, text:"a rewrre one" ,floatValue:"float-left"},
-    {id: 9, text:"a message one" ,floatValue:"float-right"},
-    {id: 10, text:"a sdfsdfs one" ,floatValue:"float-right"},
-    {id: 11, text:"a message one" ,floatValue:"float-left"},
-    {id: 12, text:"a message dsfsd" ,floatValue:"float-right"}
+    {text:"a message one" ,floatValue:"float-right"},
+    {text:"a message one" ,floatValue:"float-right"},
+    {text:"a kewer one" ,floatValue:"float-right"},
+    {text:"a message one" ,floatValue:"float-right"},
+    {text:"a message one" ,floatValue:"float-left"},
+    {text:"a message rewrwe" ,floatValue:"float-right"},
+    {text:"a message one" ,floatValue:"float-right"},
+    {text:"a rewrre one" ,floatValue:"float-left"},
+    {text:"a message one" ,floatValue:"float-right"},
+    {text:"a sdfsdfs one" ,floatValue:"float-right"},
+    {text:"a message one" ,floatValue:"float-left"},
+    {text:"a mesasdasdsage dsfsd" ,floatValue:"float-right"}
 ]
 export default messages;
