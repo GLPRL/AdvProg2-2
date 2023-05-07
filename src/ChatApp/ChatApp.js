156556ac3749617decbb4b06f50bea81a7a46c85
NaorGavriel <117026659+NaorGavriel@users.noreply.github.com>
2023-05-07 03:01:11 +0300

    small changes

--- src/ChatApp/ChatApp.js
+++ src/ChatApp/ChatApp.js
@@ -1,10 +1,12 @@
 import Left from "./Left/Left";
 import Right from "./Right/Right";
-
+import usersData from "../usersData";
+import { useState } from "react";
 function ChatApp() {
+
     return (
         <div className="container content row no-gutters text">
-            <Left />
+            <Left/>
             <Right />
         </div>
     );
--- src/ChatApp/Left/Left.js
+++ src/ChatApp/Left/Left.js
@@ -1,11 +1,11 @@
 import UserTab from "./UserTab/UserTab";
 import ContactsMenu from "./ContactsMenu/ContactsMenu";
 
-function Left() {
+function Left(props) {
     return(
         <div className="contactBox  col-sm-4">
             <UserTab />
-            <ContactsMenu />
+            <ContactsMenu/>
         </div>
     );
 }
